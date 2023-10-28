2158b3040221eb85f9e79ec77f05d1936ec5bf6a
can inan <cninnworks@gmail.com>
2023-10-28 04:49:47 +0300

    loginandregister

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -94,7 +94,8 @@ export default function Rightbar({ profile }) {
   return (
     <div className="rightbar">
       <div className="rightbarWrapper">
-        <ProfileRightbar/>
+       {profile ? <ProfileRightbar/> : <HomeRightbar/>}
+      
       </div>
     </div>
   );
